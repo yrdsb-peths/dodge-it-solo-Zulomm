529118a46c52143e62c1f2699d448f57bfcdc219
LionsonM <zulomm@outlook.com>
2025-04-22 01:00:15 +0800

    Move me

--- Me.java
+++ Me.java
@@ -12,8 +12,21 @@ public class Me extends Actor
      * Act - do whatever the Me wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    boolean top = true;
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null))
+        {
+            top = !top;
+        }
+        
+        if(top)
+        {
+            setLocation(200,800);
+        }
+        else
+        {
+            setLocation(200,200);
+        }
     }
 }
