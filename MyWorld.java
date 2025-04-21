1be35971f8e3bddbb9d681210fd91a0953116685
LionsonM <zulomm@outlook.com>
2025-04-22 00:57:08 +0800

    two object

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(1700, 1000, 1);
+        
+        Me me = new Me();
+        addObject(me, 200,200);
+        
+        Hit hit = new Hit();
+        addObject(hit, 1500,200);
     }
 }
--- hit.java
+++ hit.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class hit extends Actor
+public class Hit extends Actor
 {
     /**
      * Act - do whatever the hit wants to do. This method is called whenever
