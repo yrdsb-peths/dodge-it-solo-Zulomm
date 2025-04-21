df28a2cf609ba2f496cb5a999a00e607ab9c8516
LionsonM <zulomm@outlook.com>
2025-04-22 00:53:37 +0800

    add new

---
+++ hit.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class hit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hit extends Actor
+{
+    /**
+     * Act - do whatever the hit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
